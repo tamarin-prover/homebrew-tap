e508f4e498fafbf4f81c3f9fde1d6377e644cf7a
Katriel Cohn-Gordon <me@katriel.co.uk>
2018-02-28 13:47:02 +0000

    initial commit

---
+++ Formula/tamarin-prover.rb
@@ -0,0 +1,35 @@
+class TamarinProver < Formula
+  desc "Automated security protocol verification tool"
+  homepage "https://tamarin-prover.github.io/"
+  url "https://github.com/tamarin-prover/tamarin-prover/archive/1.2.3.tar.gz"
+  sha256 "9b637460cbadd826d8a2cdb324b660b47c6a27ff4df32e820925018c16f67796"
+  head "https://github.com/tamarin-prover/tamarin-prover.git", :branch => "develop"
+
+  depends_on "haskell-stack" => :build
+  depends_on "ocaml" => :build
+  depends_on "zlib" => :build unless OS.mac?
+  depends_on "maude"
+  depends_on "graphviz"
+  depends_on :macos => :mountain_lion
+
+  # doi "10.1109/CSF.2012.25"
+  # tag "security"
+
+  def install
+    # Let `stack` handle its own parallelization
+    # Deparallelization prevents "install: mkdir ... ghc-7.10.3/lib: File exists"
+    ENV.deparallelize
+    jobs = ENV.make_jobs
+    system "stack", "-j#{jobs}", "setup"
+    args = []
+    args << "--extra-include-dirs=#{Formula["zlib"].include}" << "--extra-lib-dirs=#{Formula["zlib"].lib}" unless OS.mac?
+    system "stack", "-j#{jobs}", *args, "install", "--flag", "tamarin-prover:threaded"
+    system "make", "sapic"
+
+    bin.install Dir[".brew_home/.local/bin/*"]
+  end
+
+  test do
+    system "#{bin}/tamarin-prover", "test"
+  end
+end
---
+++ README.md
@@ -0,0 +1,9 @@
+# Tamarin Prover Homebrew tap
+
+This is a [Homebrew](https://brew.sh/) [tap](https://docs.brew.sh/Taps) for the [Tamarin prover](https://tamarin-prover.github.io).
+
+To install:
+
+```
+brew install katrielalex/homebrew/tamarin-prover
+```
