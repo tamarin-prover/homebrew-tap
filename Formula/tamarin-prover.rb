beba6387f14e90d3e20fed12a4c6f9464fa52f36
Kevin Milner <kamilner@kamilner.ca>
2019-01-18 13:00:58 -0700

    Added Mojave bottle

--- Formula/tamarin-prover.rb
+++ Formula/tamarin-prover.rb
@@ -17,6 +17,7 @@ class TamarinProver < Formula
     cellar :any_skip_relocation
     # Looking at docs might be able to use :sierra_or_later
     sha256 "694ee78a3828a6f0f26902d49902abc8d46ad2877870a2bc07bfdeb156b9f509" => :high_sierra
+    sha256 "ff644b9cde0c9d789770dffe55e5204d3ae9678565da5a529aa758ea202c1d14" => :mojave
   end
 
   # doi "10.1109/CSF.2012.25"
