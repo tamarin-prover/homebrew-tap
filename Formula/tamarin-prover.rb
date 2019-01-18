24cfa11aaca08e03b54f4455fc7173dae22c1746
Kevin Milner <kamilner@kamilner.ca>
2019-01-18 15:10:01 -0700

    Added linux bottle

--- Formula/tamarin-prover.rb
+++ Formula/tamarin-prover.rb
@@ -18,6 +18,7 @@ class TamarinProver < Formula
     # Looking at docs might be able to use :sierra_or_later
     sha256 "694ee78a3828a6f0f26902d49902abc8d46ad2877870a2bc07bfdeb156b9f509" => :high_sierra
     sha256 "ff644b9cde0c9d789770dffe55e5204d3ae9678565da5a529aa758ea202c1d14" => :mojave
+    sha256 "6c5d2ec9fb7304e396d68c75405cfc37aa97f695f2519b5b87f64713167fb62b" => :x86_64_linux
   end
 
   # doi "10.1109/CSF.2012.25"
