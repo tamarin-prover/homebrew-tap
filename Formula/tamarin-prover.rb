8f9b7c22675b60a79cf1367a72d4de75c2b1db65
Kevin Milner <kevinmilner@improbable.io>
2020-09-10 09:22:53 +0100

    Update hashes for new bottles

--- Formula/tamarin-prover.rb
+++ Formula/tamarin-prover.rb
@@ -16,8 +16,7 @@ class TamarinProver < Formula
     root_url "https://dl.bintray.com/tamarin-prover-org/tamarin-prover"
     cellar :any_skip_relocation
     # Looking at docs might be able to use :sierra_or_later
-    sha256 "694ee78a3828a6f0f26902d49902abc8d46ad2877870a2bc07bfdeb156b9f509" => :high_sierra
-    sha256 "ff644b9cde0c9d789770dffe55e5204d3ae9678565da5a529aa758ea202c1d14" => :mojave
+    sha256 "b643fbcf5cd604fe2284e3de870140aac6d03b14651b61d792002e879aea6b45" => :catalina
     sha256 "c96a2ad6f0cb8eb29a51b1c33896732185cd770876fc3ccb16710f0889a96a9f" => :x86_64_linux
   end
 
