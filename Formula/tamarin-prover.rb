5b68337c18a997b3fadd04adf41210d722566283
Jannik Dreier <jannik.dreier@loria.fr>
2023-08-30 14:48:28 +0200

    starting update to 1.8.0

--- Formula/tamarin-prover.rb
+++ Formula/tamarin-prover.rb
@@ -1,16 +1,16 @@
 class TamarinProver < Formula
   desc "Automated security protocol verification tool"
   homepage "https://tamarin-prover.github.io/"
-  url "https://github.com/tamarin-prover/tamarin-prover/archive/1.6.1.tar.gz"
-  sha256 "2405a94d40c59030409889af1e8490617aefdd8b3cdc1bfb55a0f75b7e590d77"
+  url "https://github.com/tamarin-prover/tamarin-prover/archive/refs/tags/1.8.0.tar.gz"
+  sha256 "0c6433c461acfc675dd9437653322b8cfe6cd5fac1a14d034da8430afe1e090f"
   head "https://github.com/tamarin-prover/tamarin-prover.git", branch: "master"
 
   bottle do
-    root_url "https://github.com/tamarin-prover/tamarin-prover/releases/download/1.6.1"
-    sha256 cellar: :any,                 arm64_monterey: "34269ddaada7f142817c4aeb38dcc223b58ff42ab719d7c0447fd0dc8da9dbd2"
-    sha256 cellar: :any_skip_relocation, big_sur:        "a886014d7c2345bc2f2ea1836dd9bf1199435fcec54963f8e07e80e13fe857c6"
-    sha256 cellar: :any_skip_relocation, catalina:       "b8a142ad4961d0beb06c9c4912baacca6deb7870016a22183e40dc259e60d500"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:   "c8fb1f445c9973a62376dd16ff711e019a00e0df3fb71d2b9afefa26fecc8ffe"
+    root_url "https://github.com/tamarin-prover/tamarin-prover/releases/download/1.8.0"
+    #sha256 cellar: :any,                 arm64_monterey: "34269ddaada7f142817c4aeb38dcc223b58ff42ab719d7c0447fd0dc8da9dbd2"
+    #sha256 cellar: :any_skip_relocation, big_sur:        "a886014d7c2345bc2f2ea1836dd9bf1199435fcec54963f8e07e80e13fe857c6"
+    #sha256 cellar: :any_skip_relocation, catalina:       "b8a142ad4961d0beb06c9c4912baacca6deb7870016a22183e40dc259e60d500"
+    #sha256 cellar: :any_skip_relocation, x86_64_linux:   "c8fb1f445c9973a62376dd16ff711e019a00e0df3fb71d2b9afefa26fecc8ffe"
   end
 
   depends_on "haskell-stack" => :build
@@ -38,8 +38,8 @@ class TamarinProver < Formula
     system "stack", "-j#{jobs}", *args, "install", "--flag", "tamarin-prover:threaded"
 
     # `ocaml` building under linuxbrew needs to be single core.
-    ENV.deparallelize
-    system "make", "sapic"
+    # ENV.deparallelize
+    # system "make", "sapic"
 
     bin.install Dir[".brew_home/.local/bin/*"]
   end
