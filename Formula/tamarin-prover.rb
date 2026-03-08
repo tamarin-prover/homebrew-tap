acf30559671884fdcd1843bc8ba2fcbc675d4b5f
Kevin Milner <kamilner@kamilner.ca>
2026-03-08 18:12:57 +0800

    Updates for Tamarin 1.12.0 (Maude to 3.5.1, Tamarin 1.12.0 linux bottle) (#31)
    * Update Maude to 3.5.1
    
    * Remove zlib
    
    * Add linux bottle correctly

--- Formula/tamarin-prover.rb
+++ Formula/tamarin-prover.rb
@@ -7,11 +7,11 @@ class TamarinProver < Formula
 
   bottle do
     root_url "https://github.com/tamarin-prover/tamarin-prover/releases/download/1.12.0"
+    sha256 cellar: :any_skip_relocation, x86_64_linux: "936631c692ce9fd4c797400eff32e47d6d09e371ca251250cceac15b93f71d99"
     sha256 cellar: :any_skip_relocation, arm64_tahoe: "c4174f9f850d31243473d1bc8c04eb20117b28ec5a66276078768b58ef6ee9fd"
   end
 
   depends_on "haskell-stack" => :build
-  depends_on "zlib" => :build unless OS.mac?
   depends_on "npm" => :build
   depends_on "graphviz"
   depends_on "maude"
